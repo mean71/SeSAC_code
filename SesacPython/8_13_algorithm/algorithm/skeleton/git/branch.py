cf115835700993caa05fd3e5b8e29290874d889e
mean71 <losleepotatool@gmail.com>
2025-02-06 09:58:21 +0900

    정리작업

--- SesacPython/8_13_algorithm/algorithm/skeleton/git/branch.py
+++
@@ -1,8 +0,0 @@
-import sys 
-sys.path.append('../ADT')
-sys.path.append('../data_structure')
-
-class Branch:
-    def __init__(self, branch_name, commits):
-        self.branch_name = branch_name 
-        self.commits = commits 
